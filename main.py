c58021293cefe32c83592dbcf24b4a98eb4bf5b6
Vissercode <105940064+Vissercode@users.noreply.github.com>
2022-05-23 00:17:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+from pyrogram import Client, filters
+import time
+import config
+import chats
+
+app = Client('account',config.api_id,config.api_hash)
+
+def main():
+    with app:
+        for i in chats.chats:
+            try:
+                with open('banner.jpeg') as ft:
+                    app.send_photo(i, ft, caption=f'{config.message_text}{chats.get(i)}',)
+                
+            except:
+                try:    
+                    print(i)
+                    print(chats.get(i))
+                    app.send_message(i,f'{config.message_text}{chats.get(i)}') 
+                except:
+                    continue          
+
+if __name__ == '__main__':
+    while True:
+        main()
+        time.sleep(3000)
