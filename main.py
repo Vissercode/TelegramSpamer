713730e9f120ead87ba3d1268ea19e9b0c870880
Vissercode <105940064+Vissercode@users.noreply.github.com>
2022-05-23 20:59:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,49 @@
 from pyrogram import Client, filters
 import time
-import config
-import chats
 
-app = Client('account',config.api_id,config.api_hash)
 
+
+api_id = ''
+api_hash = ''
+app = Client('account',api_id,api_hash)
+
+message_text = """
+**🐍 Python coder 🐍**
+
+Давно ищешь человека, который мог бы реализовать твой проект?
+Не успеваешь сделать лабу? 
+Срочно нужно спарсить данные с сайта?
+Я помогу тебе.
+3-ех летний опыт в программировании на Python
+
+__🦾 Чекеры
+🦾 Парсеры
+🦾 Боты
+🦾 Работа с соц. сетями__
+
+"""
+
+
+chat = {
+-1001266598286:'📝 Связь: @vissercode\n🎩 Гарант: @scrooge_garantbot',
+-1001332422950:"📝 Связь: @vissercode\n🎩 Гарант: @lolzteam_garant_bot",
+-1001376947189:"#ищуработу"}
 def main():
     with app:
-        for i in chats.chats:
+        for i in chat:
             try:
-                with open('banner.jpeg') as ft:
-                    app.send_photo(i, ft, caption=f'{config.message_text}{chats.get(i)}',)
+                with open('banner.png') as ft:
+                    app.send_photo(i, ft, caption=f'{message_text}{chat.get(i)}',)
                 
             except:
                 try:    
                     print(i)
-                    print(chats.get(i))
-                    app.send_message(i,f'{config.message_text}{chats.get(i)}') 
+                    print(chat.get(i))
+                    app.send_message(i,f'{message_text}{chat.get(i)}') 
                 except:
                     continue          
 
 if __name__ == '__main__':
     while True:
         main()
-        time.sleep(config.sleeptime)
+        time.sleep(1800)
